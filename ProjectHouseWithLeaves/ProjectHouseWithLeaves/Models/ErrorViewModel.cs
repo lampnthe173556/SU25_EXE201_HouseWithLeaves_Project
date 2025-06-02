a5a4013d6736137a8d37fc270d51999778b9e11f
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-06-02 22:29:57 +0700

    InitProject

---
+++ ProjectHouseWithLeaves/ProjectHouseWithLeaves/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectHouseWithLeaves.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
